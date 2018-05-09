e2a2e3f07cf055472f0883729e22e2f2e8be0925
Chris <chris.anthony.yee@gmail.com>
2018-05-08 17:11:11 -0700

    stacks and queues

--- 02-stacks-and-queues/myqueue/myqueue.rb
+++ 02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,30 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :queue
 
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue.push(element)
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    removePerson = @queue.delete_at(0)
+    return removePerson
   end
 
   def empty?
+    if @queue.length === 0
+      return true
+    else
+      return false
+    end
   end
 end
--- 02-stacks-and-queues/mystack/mystack.rb
+++ 02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,22 @@ class MyStack
   end
 
   def push(item)
+    self.top = item
+    @stack.push(item)
   end
 
   def pop
+    removePerson = @stack.delete_at(-1)
+    self.top = @stack[-1]
+    return removePerson
+
   end
 
   def empty?
+    if @stack.length === 0
+      return true
+    else
+      return false
+    end
   end
 end
